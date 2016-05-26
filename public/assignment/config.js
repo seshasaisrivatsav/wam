612d2b4e8f51235df257589ada4e646d223380ee
seshasai1 <kuchibhotla.s@husky.neu.edu>
2016-05-26 15:31:39 -0400

    replaced all anchors

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -14,14 +14,49 @@
 
     function Config($routeProvider) {
         $routeProvider
+
+        /* User */
             .when("/login",{
-                templateUrl : "views/user/login.view.client.html"
+                templateUrl :"views/user/login.view.client.html"
             })
             .when("/register",{
                 templateUrl : "views/user/register.view.client.html"
             })
-            .when("/profile",{
-                templateUrl : "views/user/profile.view.client.html"
+            .when("/user/:uid",{
+                templateUrl :"views/user/profile.view.client.html"
+            })
+
+            /* Website */
+            .when("/user/:uid/website",{
+                templateUrl :"views/website/website-list.view.client.html"
+            })
+            .when("/user/:uid/website/new",{
+                templateUrl :"views/website/website-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid",{
+                templateUrl :"views/website/website-edit.view.client.html"
+            })
+            /* Page */
+            .when("/user/:uid/website/:wid/page",{
+                templateUrl :"views/page/page-list.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/new",{
+                templateUrl :"views/page/page-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid",{
+                templateUrl :"views/page/page-edit.view.client.html"
+            })
+            /* Widget */
+            .when("/user/:uid/website/:wid/page/:pid/widget",{
+                templateUrl :"views/widget/widget-list.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/new",{
+                templateUrl :"views/widget/widget-choose.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid",{
+                templateUrl :"views/widget/widget-edit.view.client.html"
             });
+        
+        
     }
 })();
