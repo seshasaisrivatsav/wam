2f1bcfa49539f8ef7a67871323016871c4a83b77
seshasai1 <kuchibhotla.s@husky.neu.edu>
2016-06-05 15:43:35 -0400

    working code!

--- public/assignment/services/flickr.service.client.js
+++ public/assignment/services/flickr.service.client.js
@@ -5,11 +5,11 @@
 
     var key = "90ea0df0685933f65ea96fab852a3023";
     var secret = "59b7fb7ceb0a467b";
-    var urlBase = "http/s://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT";
+    var urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT";
 
     function FlickrService($http) {
         var api = {
-            searchPhotos: searchPhotos 
+            searchPhotos: searchPhotos
         };
         return api;
 
