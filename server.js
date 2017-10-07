4174b94adfb5ef89f9cb0dd84b48c8830ef0a214
seshasai1@ccs.neu.edu <srivatsav@Seshas-MacBook-Pro.local>
2017-10-07 10:02:08 -0400

    Added CORS

--- server.js
+++ server.js
@@ -5,22 +5,24 @@ var passport = require('passport'); //core passport library initialized here
 var app = express();
 
 var connectionString = 'mongodb://sesha:sesha@ds113505.mlab.com:13505/webdev';
-//var connectionString = 'mongodb://127.0.0.1:27017/webdev-assignment';
+//var connectionString = 'mongodb://127.0.0.1:27017/webdev-assignment'; // To Run on local
 
 
-// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-//         process.env.OPENSHIFT_APP_NAME;
-// }
+
+// To Run on OpenShift
+/*
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+*/
+
 
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
-
-// var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/webdev-assignment');
 
 
 var bodyParser = require('body-parser');
@@ -29,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 /* VVVIP : session has to be initialized AFTER Cookie Parser */
 app.use(cookieParser());
-// app.use(session({secret: process.env.SESSION_SECRET}));
+// app.use(session({secret: process.env.SESSION_SECRET})); -- Create Session Sec
 app.use(session({secret: "aasdasd"}));
 
 
@@ -40,6 +42,16 @@ app.use(passport.session());
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+
 //require ("./test/app.js")(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
@@ -47,11 +59,12 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      =  3000;
 var port = process.env.PORT || port;
 
-/**/
+
 
 var assignment = require("./assignment/app.js");
 assignment(app);
 
+// To Run Project
 // var project = require("./project/app.js");
 // project(app);
 
